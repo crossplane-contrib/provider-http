be0dc7f75e5ce34f793a056b301b73e88a9621ea
arielsepton <64063409+arielsepton@users.noreply.github.com>
2023-12-14 21:34:15 +0000

    add local server for stable e2e tests
    Signed-off-by: arielsepton <64063409+arielsepton@users.noreply.github.com>

--- cluster/test/setup.sh
+++ cluster/test/setup.sh
@@ -10,8 +10,49 @@ cat <<EOF | ${KUBECTL} apply -f -
 apiVersion: http.crossplane.io/v1alpha1
 kind: ProviderConfig
 metadata:
-  name: http-provider
+  name: http-conf
 spec:
   credentials:
     source: InjectedIdentity
 EOF
+
+cat <<EOF | ${KUBECTL} apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: todo
+  labels:
+    app: todo
+spec:
+  replicas: 1 # Number of replicas you want
+  selector:
+    matchLabels:
+      app: todo
+  template:
+    metadata:
+      labels:
+        app: todo
+    spec:
+      containers:
+      - name: todo
+        image: danielsinai/todo:v1.0.0
+        env:
+        - name: PORT
+          value: "80"
+        ports:
+        - containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: todo
+spec:
+  type: ClusterIP
+  ports:
+  - name: "todo"
+    port: 80
+  selector:
+    app: todo
+EOF
+
+
