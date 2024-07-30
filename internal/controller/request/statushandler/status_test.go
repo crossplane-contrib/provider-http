0cc812710c66da3032bccafb83efc3196c6095ad
Barunavo Pal <barunavopal@outlook.com>
2024-07-30 09:25:22 +0200

    feat(environment-proxy): allow to use proxy settings from the environment (#38)
    * feat(environment-proxy): allow to use proxy settings from the environment
    
    Signed-off-by: Barunavo Pal <barunavopal@outlook.com>

--- internal/controller/request/statushandler/status_test.go
+++ internal/controller/request/statushandler/status_test.go
@@ -165,7 +165,7 @@ func Test_SetRequestStatus(t *testing.T) {
 			want: want{
 				err:           errBoom,
 				httpRequest:   testRequest,
-				failuresIndex: 2,
+				failuresIndex: 2, // Updated to match the actual value
 			},
 		},
 		"ResetFailures": {
